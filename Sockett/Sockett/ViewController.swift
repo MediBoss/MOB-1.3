4b7f89eee76a2e6a1ff1ef75ba5283195b8496b4
Medi Assumani <mediassumani49@gmail.com>
2019-01-21 15:04:07 -0800

    really hope we'll learn about some real time comm. using sockets

---
+++ Sockett/Sockett/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sockett
+//
+//  Created by Medi Assumani on 1/21/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
