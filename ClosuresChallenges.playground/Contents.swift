46be9febe4745e8433e7313a6c0c53581a91f1b6
Medi Assumani <mediassumani49@gmail.com>
2019-01-25 16:14:36 -0800

    completed all challenges except last

--- ClosuresChallenges.playground/Contents.swift
+++ ClosuresChallenges.playground/Contents.swift
@@ -1,18 +1,5 @@
-
-var number = [2, 43, 333, 3, 6, 40, 50, 80]
-
-var reduceMethod = number.reduce(0) { (result, current) -> Int in
-    return max(result, current)
-}
-print(reduceMethod)
-var reduceMethodShort = number.reduce(0) { max($0, $1) }
-print(reduceMethodShort)
+var numbers = [1, 2, 3, 4]
+var evenAndSquareProduct = numbers.filter({$0 % 2 == 0}).map({$0 * $0}).reduce(1, { $0 * $1 })
 
 
-
-var reduceMethodAddition = number.reduce(0) { (result, current) -> Int in
-    return result + current
-}
-print(reduceMethodAddition)
-var shortHandReduceAddition = number.reduce(0) { $0 + $1 }
-print(shortHandReduceAddition)
+print(evenAndSquareProduct)
