d1575deb1f3b318bad02e942e612843cc1232db5
Medi Assumani <mediassumani49@gmail.com>
2019-01-29 11:42:01 -0800

    Added product modle

---
+++ ProductHunt/ProductHunt/Models/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  ProductHunt
+//
+//  Created by Medi Assumani on 1/29/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+struct Product{
+    
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
