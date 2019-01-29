5917fc1ce30786f89558f1b43a17ab6acfc94bce
Medi Assumani <mediassumani49@gmail.com>
2019-01-29 13:47:23 -0800

    decodable

---
+++ ProductHunt/ProductHunt/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ProductHunt
+//
+//  Created by Medi Assumani on 1/29/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constant{
+    
+    static let API_KEY = "848896f01941d3a039c293fdb0d3d7c950f199381f0d8c87a266db266bcef080"
+    static let API_SECRET = "9434f31b08c2db4f4d4257298264a5fb22439f45d6b3ea688c2da7d2a08f1c05"
+    static let REDIRECT_URI = "https://localhost:3000/users/auth/producthunt/callback"
+    
+}
--- ProductHunt/ProductHunt/Models/Product.swift
+++ ProductHunt/ProductHunt/Models/Product.swift
@@ -15,4 +15,35 @@ struct Product{
     let tagline: String
     let votesCount: Int
     let commentsCount: Int
+    let previewImageURL: String
+}
+
+extension Product: Decodable{
+    
+    enum ProductKeys: String, CodingKey{
+        
+        case id = "id"
+        case name = "name"
+        case tagline = "tagline"
+        case voteCounts = "votes_count"
+        case commentCounts = "comments_count"
+        case thumbnailURL = "screenshot_url"
+    }
+    
+    enum PreviewImageURLKeys: String, CodingKey{
+        case imageURL = "850px"
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let productsContainer = try decoder.container(keyedBy: ProductKeys.self)
+        let productThumbnailContainer = try productsContainer.nestedContainer(keyedBy: PreviewImageURLKeys.self, forKey: .thumbnailURL)
+        
+        id = try productsContainer.decode(Int.self, forKey: .id)
+        name = try productsContainer.decode(String.self, forKey: .name)
+        tagline = try productsContainer.decode(String.self, forKey: .tagline)
+        votesCount = try productsContainer.decode(Int.self, forKey: .voteCounts)
+        commentsCount = try productsContainer.decode(Int.self, forKey: .commentCounts)
+        previewImageURL = try productThumbnailContainer.decode(String.self, forKey: .imageURL)
+    }
 }
