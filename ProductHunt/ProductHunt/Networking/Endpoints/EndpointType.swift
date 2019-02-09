e47e74f6d837552e8b0f1e3b42015375762cbd52
Medi Assumani <mediassumani49@gmail.com>
2019-02-09 13:05:00 -0800

    adding newtorking layer to a protocol oriented design

---
+++ ProductHunt/ProductHunt/Networking/Endpoints/EndpointType.swift
@@ -0,0 +1,19 @@
+//
+//  EndpointType.swift
+//  ProductHunt
+//
+//  Created by Medi Assumani on 2/9/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+// The protocol defines each API endpoint that the App might make requests to
+protocol EnpointType {
+    
+    var baseURL: URL { get }
+    var path: String { get }
+    var HTTPMethod: HTTPMethod { get }
+    var task: HTTPTask { get }
+    var headers: HTTPHeaders? { get }
+}
