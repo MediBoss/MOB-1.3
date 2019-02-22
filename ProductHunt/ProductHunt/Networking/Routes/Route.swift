b3c83e00bc6933993f1cede8e9e2d1fae0a74248
Medi Assumani <mediassumani49@gmail.com>
2019-02-21 21:30:09 -0800

    [Add] moduralized networking layer

--- ProductHunt/ProductHunt/Networking/Routes/Route.swift
+++ ProductHunt/ProductHunt/Networking/Routes/Route.swift
@@ -17,36 +17,5 @@ public enum Route: String{
     case posts = "posts/all"
     case comments = "comments"
     
-
-    
-    func getParameters() -> HTTPParameters {
-        
-        switch self {
-        case .posts:
-            return [
-                "sort_by": "votes_count",
-                "order": "desc",
-                "per_page": "20",
-                "search[featured]": "true"
-            ]
-            
-        case let .comments:
-            return [
-                "sort_by": "votes",
-                "order": "asc",
-                "per_page": "20",
-//                "search[post_id]": "\(postId)"
-            ]
-        }
-    }
-    
-    
-    func paramsToString() -> String {
-        let parameterArray = getParameters().map { key, value in
-            return "\(key)=\(value)"
-        }
-        
-        return parameterArray.joined(separator: "&")
-    }
 }
 
